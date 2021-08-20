21efc18f58aef4b87a9a9289dc501232f32580df
Ninewinger <d.v.petricio@live.com>
2021-08-20 21:21:28 +0000

    intento

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-window.onload = function(event) {
+window.onload = function() {
+
+let inputs = document.querySelector("input")
+let alert = document.querySelector("#alert");
+let submit = document.querySelector("form")
 
 }
+submit.addEventListener("click", function(event){
+    event.preventDefault();
+    
+})
+for (input of inputs) {
+    input.className = "alert-danger"
+}
 
-let alert = document.querySelector("#alert");
+
 var div = alert.parentElement;
 div.style.display = "block";
