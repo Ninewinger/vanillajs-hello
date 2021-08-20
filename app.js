56bbd7b0b9e4683de09a7560d9f0a0988961e170
Ninewinger <d.v.petricio@live.com>
2021-08-20 23:44:56 +0000

    almenos algo funciona

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-window.onload = function() {
-
-let inputs = document.querySelector("input")
+window.onload = function () { }
+let inputs = document.querySelectorAll("#inp");
 let alert = document.querySelector("#alert");
-let submit = document.querySelector("form")
+let submit = document.querySelector("#submit");
+let form = document.querySelectorAll("form")
 
+submit.addEventListener("click", function (e) {
+    e.preventDefault();
+});
+
+let inpval = []
+alert.style.display = "block";
+for (let input of inputs) {
+    inpval.push(input.nodeValue);
+    input.onsubmit = function () {
+        input.className += " alert-danger";
+        
+    }
 }
-submit.addEventListener("click", function(event){
-    event.preventDefault();
-    
-})
-for (input of inputs) {
-    input.className = "alert-danger"
-}
-
-
-var div = alert.parentElement;
-div.style.display = "block";
+console.log(inpval)
