841a16382654501a6b8b77bb4f83b18f54daf362
Ninewinger <d.v.petricio@live.com>
2021-08-22 03:37:23 +0000

    ahora con cancel se vuelve a como el principio, pero no se ponen rojos los inputs despues de los que tienen datos

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 window.onload = function () {
     let form = document.querySelector("form");
+    let cancel = document.querySelector("#cancel");
+    let inputs = document.querySelectorAll("input");
+    let alert = document.querySelector("#alert");
+    let texta = document.querySelector("#texta");
+    let state = document.querySelector("#state");
+    
+    let inp = []
+
+    for (input of inputs) {
+        inp.push(input)
+    }
+    inp.push(texta)
+    inp.push(state)
 
     form.addEventListener("submit", function (e) {
         e.preventDefault();
-        let inputs = document.querySelectorAll("input");
-        let alert = document.querySelector("#alert");
-        let texta = document.querySelector("#texta");
-        let state = document.querySelector("#state");
-        let cancel = document.querySelector("#cancel");
-        let inp = []
-        inp.push(texta, state)
-        for (input of inputs) {
-            inp.push(input)
-        }
-        for (data of inp){    
+
+        for (data of inp) {
             if (data.value === "") {
                 data.className += " alert-danger";
                 alert.style.display = "block";
-            }else{
+            } else {
                 e.target.submit();
             }
         }
     });
-
+    cancel.addEventListener("click", function () {
+        let inputs = document.querySelectorAll("input");
+        let alert = document.querySelector("#alert");
+        for (input of inp) {
+            input.value = ""
+            input.className = input.className.replace("alert-danger", "")
+            alert.style.display = "none";
+        }
+    })
 
 }
