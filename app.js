7f19680e9f4f5bb7864902dcd73cee6298f5f975
Ninewinger <d.v.petricio@live.com>
2021-08-23 01:28:56 +0000

    se mandogit add .

--- app.js
+++ app.js
@@ -5,23 +5,16 @@ window.onload = function () {
     let alert = document.getElementById("alert");
 
     form.addEventListener("submit", function (e) {
-        e.preventDefault();
-
         for (let input of inputs) {
             if (input.value === "") {
+                e.preventDefault();
                 input.classList.add("alert-danger")
                 alert.style.display = "block";
-            } else {
-                form.removeEventListener
-/*                 form.addEventListener("submit", function (e) {
-                    e.target.submit();
-                }); */
-            }
+            } 
         }
     });
     cancel.addEventListener("click", function () {
         alert.style.display = "none";
-
         for (let input of inputs) {
             input.value = ""
             input.classList.remove("alert-danger")
