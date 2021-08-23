2e5d4607ce7af4a45305f3d2649e1744cb189c8b
Ninewinger <d.v.petricio@live.com>
2021-08-23 22:03:30 +0000

    alfin terminada de verdad

--- app.js
+++ app.js
@@ -1,17 +1,129 @@
 window.onload = function () {
-    let form = document.querySelector("form");
+    let form = document.getElementsByTagName("form")[0];
     let cancel = document.getElementById("cancel");
     let inputs = document.getElementsByName("input");
     let alert = document.getElementById("alert");
 
     form.addEventListener("submit", function (e) {
-        for (let input of inputs) {
-            if (input.value === "") {
-                e.preventDefault();
-                input.classList.add("alert-danger")
-                alert.style.display = "block";
-            }
+        let hasError = false;
+        
+        let regcard = /[0-9]{13,16}/
+        let { card } = form;
+        if (card.value === "" || !regcard.test(card.value)) {
+            e.preventDefault();
+            card.classList.add("alert-danger");
+            alert.style.display = "block";
+            hasError = true;
+        } else {
+            card.classList.remove('alert-danger');
+            card.classList.add('is-valid');
+            alert.style.display = "none";
         }
+
+        let regcvc = /\d{4}/
+        let {cvc} = form;
+        if (cvc.value === "" || !regcvc.test(cvc.value)) {
+            e.preventDefault();
+            cvc.classList.add("alert-danger");
+            alert.style.display = "block";
+            hasError = true;
+        } else {
+            cvc.classList.remove('alert-danger');
+            cvc.classList.add('is-valid');
+            alert.style.display = "none";
+        }
+
+        let regamount = /\d/
+        let {amount} = form;
+        if (amount.value === "" || !regamount.test(amount.value)) {
+            e.preventDefault();
+            amount.classList.add("alert-danger");
+            alert.style.display = "block";
+            hasError = true;
+        } else {
+            amount.classList.remove('alert-danger');
+            amount.classList.add('is-valid');
+            alert.style.display = "none";
+        }
+
+        let regname = /[a-zA-Z]+/
+        let {name} = form;
+        if (name.value === "" || !regname.test(name.value)) {
+            e.preventDefault();
+            name.classList.add("alert-danger");
+            alert.style.display = "block";
+            hasError = true;
+        } else {
+            name.classList.remove('alert-danger');
+            name.classList.add('is-valid');
+            alert.style.display = "none";
+        }
+
+        let reglname = /[a-zA-Z]+/
+        let {lname} = form;
+        if (lname.value === "" || !reglname.test(lname.value)) {
+            e.preventDefault();
+            lname.classList.add("alert-danger");
+            alert.style.display = "block";
+            hasError = true;
+        } else {
+            lname.classList.remove('alert-danger');
+            lname.classList.add('is-valid');
+            alert.style.display = "none";
+        }
+
+        let regcity = /[a-zA-Z]+/
+        let {city} = form;
+        if (city.value === "" || !regcity.test(city.value)) {
+            e.preventDefault();
+            city.classList.add("alert-danger");
+            alert.style.display = "block";
+            hasError = true;
+        } else {
+            city.classList.remove('alert-danger');
+            city.classList.add('is-valid');
+            alert.style.display = "none";
+        }
+
+        let {state} = form;
+        if (state.value === "") {
+            e.preventDefault();
+            state.classList.add("alert-danger");
+            alert.style.display = "block";
+            hasError = true;
+        } else {
+            state.classList.remove('alert-danger');
+            state.classList.add('is-valid');
+            alert.style.display = "none";
+        }
+
+        let regpostal = /\d/
+        let {postal} = form;
+        if (postal.value === "" || !regpostal.test(postal.value)) {
+            e.preventDefault();
+            postal.classList.add("alert-danger");
+            alert.style.display = "block";
+            hasError = true;
+        } else {
+            postal.classList.remove('alert-danger');
+            postal.classList.add('is-valid');
+            alert.style.display = "none";
+        }
+
+        let {texta} = form;
+        if (texta.value === "") {
+            e.preventDefault();
+            texta.classList.add("alert-danger");
+            alert.style.display = "block";
+            hasError = true;
+        } else {
+            texta.classList.remove('alert-danger');
+            texta.classList.add('is-valid');
+            alert.style.display = "none";
+        }
+
+
+
     });
     cancel.addEventListener("click", function () {
         alert.style.display = "none";
